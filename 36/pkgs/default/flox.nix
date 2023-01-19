c4bc851f8a9a8795a89eec80b2800871ac45a146
Julien Urraca <julien@floxdev.com>
2023-01-19 17:48:00 -0600

    julien installed stable.nixpkgs-flox.yarn
    flox install yarn
      stable.nixpkgs-flox.yarn: ∅ -> 1.22.19

---
+++ 36/pkgs/default/flox.nix
@@ -0,0 +1,34 @@
+{...}: {
+  packages = {
+    nixpkgs-flox = {
+      # hello = {version = "2.12.1";};
+    };
+  };
+  packages.nixpkgs.vscode = {};
+  packages.nixpkgs-flox.glow = {};
+  packages.nixpkgs-flox.yarn = {};
+  packages.nixpkgs-flox.vlc = {};
+  packages.nixpkgs-flox.tree = {};
+  packages.nixpkgs-flox.ruby_3_1 = {};
+  packages.nixpkgs-flox.ripgrep = {};
+  packages.nixpkgs-flox.python3 = {};
+  packages.nixpkgs-flox.jq = {};
+  packages.nixpkgs-flox.hello = {};
+  packages.nixpkgs-flox.fzf = {};
+  packages.nixpkgs-flox.ffmpeg = {};
+  packages.nixpkgs-flox.elixir = {};
+  packages.nixpkgs-flox.direnv = {};
+  packages.nixpkgs-flox.cachix = {};
+  shell = {
+    aliases = {
+       gg = "git grep";
+    };
+    # POSIX shell hook, except curly braces have to be escaped like ``${
+    hook = ''
+      # echo "Supercharged by flox!" 1>&2
+    '';
+  };
+  environmentVariables = {
+    # LANG = "en_US.UTF-8";
+  };
+}
